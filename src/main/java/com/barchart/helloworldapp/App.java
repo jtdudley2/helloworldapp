54daf29bc6925a1f704d58cf11182d4b0ba17cb2
Joel Dudley <joel.dudley@barchart.com>
2014-11-06 08:59:06 -0600

    Update to GuiceLauncher

--- src/main/java/com/barchart/helloworldapp/App.java
+++ src/main/java/com/barchart/helloworldapp/App.java
@@ -2,22 +2,24 @@ package com.barchart.helloworldapp;
 
 import java.util.Set;
 
-import com.barchart.util.guice.Component;
-import com.barchart.util.guice.GuiceApp;
+import com.barchart.util.guice.GuiceLauncher;
 import com.google.inject.Inject;
 
-@Component("App")
-public class App implements GuiceApp {
+public final class App {
 
 	@Inject
 	private Set<MyComponent> components;
 
-	public void run(String[] args) throws Exception {
+	private void start() {
 		System.out.println("Hello world");
 		for (MyComponent c : components) {
 			c.saySomething();
 		}
+	}
 
+	public static void main(String[] args) throws Exception {
+		App app = GuiceLauncher.configure(App.class);
+		app.start();
 	}
 
 }
